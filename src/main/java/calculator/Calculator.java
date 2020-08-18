a40bf166cf6ef1f8fc1f3628e1ce485d2effcd65
jahidul2543 <jahidul2543@gmail.com>
2020-08-18 19:03:11 -0400

    maven demo

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,16 @@
+package calculator;
+
+public class Calculator {
+
+
+    public int add(int x, int y){
+        int result = x + y;
+        return result;
+    }
+    public int deduct(int x, int y){
+        int result = x - y;
+        return result;
+    }
+    // I want to check/test add() and deduct() method programatically
+
+}
